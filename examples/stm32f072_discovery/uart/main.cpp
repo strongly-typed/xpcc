47cc60c40406f238569d7319ed9c0220ea0a35c9
Electronic Kiwi <electron.kiwi@gmail.com>
2015-06-04 12:58:50 +0200

    [stm32f072] add rudimentary support for USART1

---
+++ examples/stm32f072_discovery/uart/main.cpp
@@ -0,0 +1,46 @@
+#include <xpcc/architecture.hpp>
+#include <xpcc/debug/logger.hpp>
+
+// ----------------------------------------------------------------------------
+// Set the log level
+#undef	XPCC_LOG_LEVEL
+#define	XPCC_LOG_LEVEL xpcc::log::INFO
+
+#include "../stm32f072_discovery.hpp"
+
+
+// Create an IODeviceWrapper around the Uart Peripheral we want to use
+xpcc::IODeviceWrapper< Usart1, xpcc::IOBuffer::BlockIfFull > loggerDevice;
+
+// Set all four logger streams to use the UART
+xpcc::log::Logger xpcc::log::debug(loggerDevice);
+xpcc::log::Logger xpcc::log::info(loggerDevice);
+xpcc::log::Logger xpcc::log::warning(loggerDevice);
+xpcc::log::Logger xpcc::log::error(loggerDevice);
+
+
+// Dummy Clock
+struct DummyClock {
+	static constexpr int Usart1 = 8 * 1000 * 1000;
+};
+
+MAIN_FUNCTION
+{
+	// initialize Uart1 for XPCC_LOG_
+	GpioOutputA9::connect(Usart1::Tx);
+	GpioInputA10::connect(Usart1::Rx, Gpio::InputType::PullUp);
+	Usart1::initialize<DummyClock, 115200>(12);
+
+	// Use the logging streams to print some messages.
+	// Change XPCC_LOG_LEVEL above to enable or disable these messages
+	XPCC_LOG_DEBUG   << "debug"   << xpcc::endl;
+	XPCC_LOG_INFO    << "info"    << xpcc::endl;
+	XPCC_LOG_WARNING << "warning" << xpcc::endl;
+	XPCC_LOG_ERROR   << "error"   << xpcc::endl;
+
+	while (1)
+	{
+	}
+
+	return 0;
+}
