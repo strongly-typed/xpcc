efd7b215f36d6811dc1a38112625cfb49eb271d3
strongly-typed <stronglytyp3d@gmail.com>
2016-06-25 06:05:00 +0200

    [STM32L4] Enable UART1..5
    USART 2 tested with RX and TX with 9600, 115200 and 230400 baud.

--- examples/stm32l476_discovery/blink/main.cpp
+++ examples/stm32l476_discovery/blink/main.cpp
@@ -10,10 +10,22 @@ main()
 
 	LedRed::set();
 
+	// Enable USART 2
+	GpioOutputA2::connect(xpcc::stm32::Usart2::Tx);
+	GpioInputA3::connect(xpcc::stm32::Usart2::Rx, Gpio::InputType::PullUp);
+	Usart2::initialize<Board::systemClock, 230400>(12);
+
 	while (true)
 	{
+		static uint8_t c = 'A';
 		LedGreen::toggle();
 		LedRed::toggle();
+
+		Usart2::write(c);
+		++c;
+		if (c > 'Z') {
+			c = 'A';
+		}
 		xpcc::delayMilliseconds(Button::read() ? 250 : 500);
 	}
 
